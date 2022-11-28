cbd79fd798d635d79d4dfd1d1c4228320d51b06b
DG-deJeu <daniel.dejeu@gmail.com>
2022-11-28 17:31:18 +0100

    UnitTests

---
+++ src/backend/PortaalBackend/PortaalBackend.Tests/UserServiceTests/CreateUser.cs
@@ -0,0 +1,30 @@
+﻿namespace PortaalBackend.Tests.UserServiceTests;
+
+[ExcludeFromCodeCoverage]
+public class CreateUser
+{
+    private readonly UserService userService;
+    private readonly Mock<IRepository<User>> mockRepository;
+
+    public CreateUser()
+    {
+        mockRepository = new Mock<IRepository<User>>();
+        userService = new UserService(mockRepository.Object);
+    }
+
+
+    [Fact]
+    public async Task Should_Call_Repository()
+    {
+        // Arrange
+        User input = new() { Id = -1 };
+        mockRepository.Setup(y => y.Create(input)).ReturnsAsync(input);
+
+        // Act
+        User answer = await userService.CreateUser(input);
+
+        // Assert
+        mockRepository.Verify(y => y.Create(input), Times.Once);
+        Assert.Equal(input.Id, answer.Id);
+    }
+}
