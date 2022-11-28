23f577d3b5f51ed6c3ac4c7efdecc66eb9ad6cde
DG-deJeu <daniel.dejeu@gmail.com>
2022-11-28 17:31:06 +0100

    UserController - Create Student UserService GetByEmail & CreateUser

--- src/backend/PortaalBackend/PortaalBackend.Business/Services/UserService.cs
+++ src/backend/PortaalBackend/PortaalBackend.Business/Services/UserService.cs
@@ -1,9 +1,26 @@
 ﻿using PortaalBackend.Domain.Interfaces;
+using PortaalBackend.Domain.Models;
 
 namespace PortaalBackend.Business.Services
 {
     public class UserService : IUserService
     {
+        private readonly IRepository<User> repository;
 
+        public UserService(IRepository<User> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            User createdUser = await repository.Create(user);
+            return createdUser;
+        }
+
+        public User? GetUserByEmail(string email)
+        {
+            return repository.GetAll().SingleOrDefault(x => x.Email == email);
+        }
     }
 }
