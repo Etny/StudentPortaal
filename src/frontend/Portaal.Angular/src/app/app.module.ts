d1fe7eb82e9727a76cb200aecdbf190f059efd6c
DG-deJeu <daniel.dejeu@gmail.com>
2022-11-29 13:40:53 +0100

    app.module fixed duplicate

--- src/frontend/Portaal.Angular/src/app/app.module.ts
+++ src/frontend/Portaal.Angular/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
-import { AssignmentDetailsComponent } from './Components/assignment-details/assignment-details.component';
 import { LoginComponent } from './Components/login/login.component';
 import { CreateStudentComponent } from './Components/create-student/create-student.component'
 import { AssignmentDetailsComponent } from './Components/assignment-details/assignment-details.component'
